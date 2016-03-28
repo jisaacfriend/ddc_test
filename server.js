29cfcbe77290f4bfdc008db0a47ff714d70153d0
root <root@test.experimentwith.info>
2016-03-27 21:13:06 -0400

    Adding app.js and initial mod of prescreen.html

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-	path = require('path');
-
-var app = express();
-
-app.use('/', express.static(__dirname + '/'));
-
-app.listen(80, function() { console.log('listening'); });
